d111e85ae27c98c654b9e7387e184bc56d308152
Chris Mecrow <mecrow@live.co.uk>
2021-10-27 16:57:57 +0000

    import code for canvas image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
+var url = "../assets/images/frost.webp";
+var canvas = document.getElementById("canvas-image");
+var ctx = canvas.getContext('2d');
+var img = new Image();
+img.src = url;
+
+img.onload = function () {
+  var width =  img.width;
+  var height = img.height;
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(img, 0, 0, width, height);
+};
+
+var isPress = false;
+var old = null;
+canvas.addEventListener('mousedown', function (e){
+  isPress = true;
+  old = {x: e.offsetX, y: e.offsetY};
+});
+canvas.addEventListener('mousemove', function (e){
+  if (isPress) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    ctx.globalCompositeOperation = 'destination-out';
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.lineWidth = 20;
+    ctx.beginPath();
+    ctx.moveTo(old.x, old.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    old = {x: x, y: y};
+
+  }
+});
+canvas.addEventListener('mouseup', function (e){
+  isPress = false;
+});
+
 // Variables for buttons to select themes
 
-let img = document.getElementById("game-area");
 let forestButton = document.getElementById("selection-one");
 let mountainButton = document.getElementById("selection-two");
 let beachButton = document.getElementById("selection-three");
